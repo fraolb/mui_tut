71e1a88cbb1d492121167d753eb28e385c837971
fraolb <Fraolbereket@gmail.com>
2023-06-06 11:07:56 +0300

    tutorial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Typography, Button } from "@mui/material";
+import { green, red } from "@mui/material/colors";
+import { CssBaseline } from "@mui/material";
+import { lightTheme, darkTheme } from "./theme";
+import Box from "@mui/material/Box";
+import { useState } from "react";
+import { styled } from "@mui/material";
+
+const CustomizedTypo = styled(Typography)(({theme})=>({
+  color: theme.palette.primary.main,
+  '&:hover':{
+    color: theme.palette.secondary.main
+  }
+}))
 
 function App() {
+  const [theme, setTheme] = useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme ? lightTheme : darkTheme}>
+      <Box bgcolor='bw.main' sx={{height:'100vh'}}>
+        <Typography color="primary.main" variant="h1">
+          Hello World
+        </Typography>
+        <Typography color="secondary.main" variant="h1">
+          Hello World
+        </Typography>
+        <Button variant="contained" onClick={()=>setTheme(!theme)}>Submit</Button>
+        <CustomizedTypo variant="h1">Jhon Doe</CustomizedTypo>
+      </Box>
+    </ThemeProvider>
   );
 }
 
