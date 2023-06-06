fb42e5dec08ff695f4b3b1e44576fb732d54f8e4
fraolb <Fraolbereket@gmail.com>
2023-06-06 17:58:02 +0300

    List

--- src/App.js
+++ src/App.js
@@ -1,35 +1,58 @@
-import "./App.css";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { Typography, Button } from "@mui/material";
-import { green, red } from "@mui/material/colors";
-import { CssBaseline } from "@mui/material";
-import { lightTheme, darkTheme } from "./theme";
-import Box from "@mui/material/Box";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemButton,
+  ListItemIcon,
+  ListItemAvatar, 
+  Avatar,
+  Collapse
+} from "@mui/material";
+import DraftIcon from '@mui/icons-material/Drafts'
 import { useState } from "react";
-import { styled } from "@mui/material";
-
-const CustomizedTypo = styled(Typography)(({theme})=>({
-  color: theme.palette.primary.main,
-  '&:hover':{
-    color: theme.palette.secondary.main
-  }
-}))
+import ExpandLess from '@mui/icons-material/ExpandLess'
+import ExpandMore from '@mui/icons-material/ExpandMore'
 
 function App() {
-  const [theme, setTheme] = useState(true)
+  const [open , setOpen] = useState(false)
+  const handleClick=()=>{
+    setOpen(!open)
+  }
   return (
-    <ThemeProvider theme={theme ? lightTheme : darkTheme}>
-      <Box bgcolor='bw.main' sx={{height:'100vh'}}>
-        <Typography color="primary.main" variant="h1">
-          Hello World
-        </Typography>
-        <Typography color="secondary.main" variant="h1">
-          Hello World
-        </Typography>
-        <Button variant="contained" onClick={()=>setTheme(!theme)}>Submit</Button>
-        <CustomizedTypo variant="h1">Jhon Doe</CustomizedTypo>
+    <div style={{ backgroundColor: "#E8ECF4", height: "100vh", padding: 10 }}>
+      <Box sx={{ maxWidth: 360, bgcolor: "white" }}>
+        <List>
+          <ListItem>
+            <ListItemButton>
+              <ListItemIcon>
+                <DraftIcon />
+              </ListItemIcon>
+              <ListItemText primary="Primary" secondary="secondary" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar alt='person' src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" />
+            </ListItemAvatar>
+            <ListItemText primary="This is secondary text" />
+          </ListItem>
+          <ListItem>
+            <ListItemButton onClick={handleClick}>
+              <ListItemText primary="inbox" />
+              {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+          </ListItem>
+          <Collapse in={open} timeout="auto">
+            <List component="div" disablePadding>
+              <ListItem>
+                <ListItemText primary="starred" />
+              </ListItem>
+            </List>
+          </Collapse>
+        </List>
       </Box>
-    </ThemeProvider>
+    </div>
   );
 }
 
