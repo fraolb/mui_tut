09815f0f80708df65d372b02cdf53b3efb751c1a
fraolb <Fraolbereket@gmail.com>
2023-06-09 10:08:04 +0300

    snackbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Snackbar, Button, IconButton, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle,  } from "@mui/material";
+import { Snackbar, Button, IconButton } from "@mui/material";
 import { useState } from "react";
 import CloseIcon from '@mui/icons-material/Close'
 
@@ -11,21 +11,26 @@ function App() {
     setOpen(false);
   };
 
-
+  const action =(
+    <>
+      <Button color="secondary" size="small" onClick={handleClose}>
+        UNDO
+      </Button>
+      <IconButton size="small" onClick={handleClose}>
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </>
+  )
   return (
     <div style={{ backgroundColor: "#E8ECF4", height: "100vh", padding: 10 }}>
-      <Button variant="contained" onClick={handleClick}>Open Dialog</Button>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Simple Dialog</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            This is a simple dialog example.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Close</Button>
-        </DialogActions>
-      </Dialog>
+      <Button variant="contained" onClick={handleClick}>Open Snackbar</Button>
+      <Snackbar
+        open={open}
+        autoHideDuration={5000}
+        onClose={handleClose}
+        message="This is snackbar!"
+        action={action}
+      />
     </div>
   );
 }
